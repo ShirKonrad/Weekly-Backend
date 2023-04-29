786b201d2d6624eae1e7a0d1b3149aa11bd83f1e
Shir Konrad <shir.konrad@gmail.com>
2023-04-29 18:53:49 +0300

    Add(task) get all tasks and set task done

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,8 +1,28 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { DataNotFoundError } from "../errors/dataNotFoundError";
+import { getUserId } from "../helpers/currentUser";
+import { getAllTasksByUserId, setDone } from "../services/task";
 
 
 const router = Router();
 
-// add task
+router.get("/all", async (req: Request, res: Response, next) => {
+    const tasks = await getAllTasksByUserId(getUserId(req), true);
+    if (!tasks) {
+        next(new DataNotFoundError("Tasks"));
+    } else {
+        return res.status(200).send(tasks);
+    }
+})
+
+router.put("/setdone/:id", async (req: Request, res: Response) => {
+    const updatedTask = await setDone(parseInt(req.params.id), getUserId(req));
+
+    if (!updatedTask) {
+        throw new DataNotFoundError("Task");
+    } else {
+        return res.status(200).send(updatedTask);
+    }
+});
 
 export { router as taskRouter };
