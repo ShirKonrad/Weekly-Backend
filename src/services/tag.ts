84f9e2b108b416355d92eb3dd029ca8fadc13243
ofir-hazan <322815093@idf.il>
2023-06-10 18:49:32 +0300

    add tag logic

--- src/services/tag.ts
+++ src/services/tag.ts
@@ -12,7 +12,12 @@ export async function getTagById(id: number, userId: number) {
   return await Tag.findOne({
     where: {
       id: id,
-      user: { id: userId }
-    }
+      user: { id: userId },
+    },
   });
+}
+
+export async function addNewTag(tag: Tag, userId: number) {
+  const newTag = Tag.create({ ...tag, user: { id: userId } });
+  return await Tag.insert(newTag);
 }
