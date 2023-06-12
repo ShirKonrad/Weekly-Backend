2eb9b4ababd7b8dd02692e24a2ae543739cb56b9
Shir Konrad <shir.konrad@gmail.com>
2023-06-13 00:38:06 +0300

    Add(errors): add client message error

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,4 +1,10 @@
+import { ClientError } from "../errors/clientMessageError";
+
 export type TaskAssignment = {
     taskId: number;
     assignment: Date;
+}
+
+export type ClientErrors = {
+    [key: string]: ClientError
 }
