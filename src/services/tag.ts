0ac14fe3977e379860b869daf3c752fd8d86b916
ofir hazan <80819608+ofir-hazan@users.noreply.github.com>
2023-06-14 21:52:25 +0300

    Edit delete tag flow (#22)
    * delete tag logic
    
    * update tag logic
    
    * fix user err msg
    
    * fix conflicts
    
    * clean user router
    
    ---------
    
    Co-authored-by: ofir-hazan <322815093@idf.il>

--- src/services/tag.ts
+++ src/services/tag.ts
@@ -21,3 +21,25 @@ export async function addNewTag(tag: Tag, userId: number) {
   const newTag = Tag.create({ ...tag, user: { id: userId } });
   return await Tag.insert(newTag);
 }
+
+export async function deleteTag(tagId: number) {
+  return await Tag.delete(tagId);
+}
+
+export async function updateTag(newTag: Tag, userId: number) {
+  const tag = await Tag.findOne({
+    where: {
+      id: newTag.id,
+      user: { id: userId },
+    },
+  });
+
+  if (tag) {
+    tag.name = newTag.name;
+    tag.color = newTag.color;
+
+    return await Tag.save(tag);
+  } else {
+    return undefined;
+  }
+}
