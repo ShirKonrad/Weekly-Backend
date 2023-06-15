dff020ff3a417ef1e866fabc3ee0b51b507812e8
Shir Konrad <shir.konrad@gmail.com>
2023-06-16 00:15:34 +0300

    fix build settings

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,14 @@ import { router } from "./routes"
 import cors from "cors";
 import { assignmentsUpdate } from "./services/job";
 const cron = require("node-cron");
+const compression = require("compression");
 
 const app = express();
 
+app.use(compression());
 app.use(express.json());
 app.use(cors());
+
 
 app.use(router);
 
