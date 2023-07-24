32bda5395a5c97de8056e2f1c9dcec3b8c7be4da
Omer Damari <80820783+omerd318@users.noreply.github.com>
2023-07-24 20:41:24 +0300

    Fix(Swagger): Change server url to env var (#51)

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,42 @@
 import express from "express";
 import cors from "cors";
-import { router } from "./routes"
+import { router } from "./routes";
 
 const compression = require("compression");
 const app = express();
 
-if (process.env.NODE_ENV == "development") {
-    const swaggerUI = require("swagger-ui-express")
-    const swaggerJsDoc = require("swagger-jsdoc")
-    const options = {
-        definition: {
-            openapi: "3.0.0",
-            info: {
-                title: "Weekly API",
-                version: "1.0.0",
-                description: "API for Weekly app.",
-            },
-            servers: [{url: "http://localhost:" + process.env.SERVER_PORT},],
-            components: {
-                securitySchemes: {
-                    bearerAuth: {
-                        type: 'http',
-                        scheme: 'bearer',
-                        bearerFormat: 'JWT',
-                        name: 'token',
-                        in: 'header'
-                    }
-                }
-            },
-            security: [{
-                bearerAuth: []
-            }]
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Weekly API",
+      version: "1.0.0",
+      description: "API for Weekly app.",
+    },
+    servers: [{ url: process.env.SERVER_URL }],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+          name: "token",
+          in: "header",
         },
-        apis: ["src/routes/*.ts"],
-    };
-    const specs = swaggerJsDoc(options);
-    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
- } 
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["src/routes/*.ts"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(compression());
 app.use(express.json());
